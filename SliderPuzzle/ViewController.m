5a8d908028d7932663ece38a4e386b8f1af4cd85
richy486 <richy486@gmail.com>
2014-04-27 12:16:47 +1000

    Remvoed unnessassary loops

--- SliderPuzzle/ViewController.m
+++ SliderPuzzle/ViewController.m
@@ -34,42 +34,28 @@ NSInteger const PIECES_PER_SIDE = 4;
     NSInteger index = 0;
     for (NSInteger iY = 0; iY < PIECES_PER_SIDE; ++iY) {
         for (NSInteger iX = 0; iX < PIECES_PER_SIDE; ++iX) {
-        
-            CGRect pieceRect = CGRectMake(iX * pieceSize * fullImage.scale
-                                          , iY * pieceSize * fullImage.scale
-                                          , pieceSize * fullImage.scale
-                                          , pieceSize * fullImage.scale);
             
-            CGImageRef imageRef = CGImageCreateWithImageInRect([fullImage CGImage], pieceRect);
-            UIImage *pieceImage = [UIImage imageWithCGImage:imageRef scale:fullImage.scale orientation:fullImage.imageOrientation];
-            CGImageRelease(imageRef);
-            
-            Piece *piece = [[Piece alloc] initWithImage:pieceImage andOriginalIndex:index];
-            
-//            piece.center = CGPointMake(CGRectGetMidX(self.view.frame) - ((PIECES_PER_SIDE/2) * pieceSize) + (pieceSize/2) + (iX * pieceSize)
-//                                       , CGRectGetMidY(self.view.frame) - ((PIECES_PER_SIDE/2) * pieceSize) + (pieceSize/2) + (iY * pieceSize));
-//            piece.gridPosition = piece.center;
-            [pieces addObject:piece];
+            if (index < PIECES_PER_SIDE * PIECES_PER_SIDE - 1) {
+                CGRect pieceRect = CGRectMake(iX * pieceSize * fullImage.scale
+                                              , iY * pieceSize * fullImage.scale
+                                              , pieceSize * fullImage.scale
+                                              , pieceSize * fullImage.scale);
+                
+                CGImageRef imageRef = CGImageCreateWithImageInRect([fullImage CGImage], pieceRect);
+                UIImage *pieceImage = [UIImage imageWithCGImage:imageRef scale:fullImage.scale orientation:fullImage.imageOrientation];
+                CGImageRelease(imageRef);
+                
+                Piece *piece = [[Piece alloc] initWithImage:pieceImage andOriginalIndex:index];
+
+                [pieces addObject:piece];
+                [self.view addSubview:piece];
+            }
             
             ++index;
         }
     }
     
-    // Remove the last piece
-    [pieces removeObjectAtIndex:[pieces count] -1];
-    
-
-    NSInteger i = 0;
-    for (Piece *piece in pieces) {
-        [self.view addSubview:piece];
-        ++i;
-    }
-    
     self.boardController = [[BoardController alloc] initWithPieces:pieces andViewRect:self.view.frame];
-    
-    NSLog(@"pieces per side: %ld", (long)[self.boardController piecesPerSide]);
-    
-
 }
 
 #pragma mark memory man
